2df3651f66547504a35112ffb437caff1ccec657
Dan Hart <dan-hart@users.noreply.github.com>
2022-05-25 17:11:50 -0400

    Add GET test

--- SwiftCodeGeneratorTests/Tests/SwiftCodeGeneratorTestsTests/SwiftCodeGeneratorTestsTests.swift
+++ SwiftCodeGeneratorTests/Tests/SwiftCodeGeneratorTestsTests/SwiftCodeGeneratorTestsTests.swift
@@ -1,47 +1,35 @@
 import XCTest
 import class Foundation.Bundle
 
+@available(macOS 12.0, *)
 final class SwiftCodeGeneratorTestsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    /// Addresses
+    /// get https://fakerapi.it/api/v1/addresses
+    func getAddressesRequest() async throws {
+        let parameters = [
+            "_quantity":"100",
+            "_locale":"en_US",
+        ]
 
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
+        var components = URLComponents(string: "https://fakerapi.it/api/v1/addresses")!
+
+        for parameter in parameters {
+        if parameter.value.lowercased() == "null" { continue }
+            components.queryItems?.append(URLQueryItem(name: parameter.key, value: parameter.value))
         }
 
-        // Mac Catalyst won't have `Process`, but it is supported for executables.
-        #if !targetEnvironment(macCatalyst)
-
-        let fooBinary = productsDirectory.appendingPathComponent("SwiftCodeGeneratorTests")
-
-        let process = Process()
-        process.executableURL = fooBinary
-
-        let pipe = Pipe()
-        process.standardOutput = pipe
-
-        try process.run()
-        process.waitUntilExit()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
-
-        XCTAssertEqual(output, "Hello, world!\n")
-        #endif
+        // Create request with HTTP method
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "get".uppercased()
+        
+        let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
+        print("Response: \(response.debugDescription)")
+        print("Data: \(String(data: data, encoding: .utf8) ?? "Unable to render data as UTF-8 string")")
+        //let json = try JSONSerialization.jsonObject(with: data, options: [])
+        //print("JSON: \(json)")
     }
-
-    /// Returns path to the built products directory.
-    var productsDirectory: URL {
-      #if os(macOS)
-        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-            return bundle.bundleURL.deletingLastPathComponent()
-        }
-        fatalError("couldn't find the products directory")
-      #else
-        return Bundle.main.bundleURL
-      #endif
+    
+    func testGet() async throws {
+        try await getAddressesRequest()
     }
 }
